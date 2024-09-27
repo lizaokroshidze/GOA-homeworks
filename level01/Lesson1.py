c51cfb26634756c9e2ab698793f12794d57c2457
lizaokroshidze <liziokroshidze1994@gmail.com>
2024-09-27 14:20:17 +0400

    Update Lesson1.py

--- level01/Lesson1.py
+++ level01/Lesson1.py
@@ -3,9 +3,9 @@ name = "Liza" # str
 
 surname = "Okroshidze" # str
 
-age = "19" # int
+age = 19 # int
 
-height = "174" # float
+height = 1.74 # float
 
 print("My name is" + " "  + ", My last name is " + surname + ",i am " + str(age) + " years old , and my height is " + str(height) + " centimetres.")
 
