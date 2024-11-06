a5250eef9124585a76fdd5c575aebba9e8ad4991
lizaokroshidze <liziokroshidze1994@gmail.com>
2024-11-06 22:18:24 +0400

    commit

---
+++ level12 /classwork/cw1.py
@@ -0,0 +1,18 @@
+#for loop 
+#while loop
+
+#iteration - იტერაცია - 
+
+#loop
+#ციკლი
+
+#for i in range(20):
+ #   print("goa best")
+
+for i in range (20):
+     if i % 2 == 0:
+         print(str(i)+ " even")
+     else:
+          print(str(i)+" odd")
+        
+
