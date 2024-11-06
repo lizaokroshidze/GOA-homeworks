fddb72e166d4d2748418e5ef8289dee518975e50
lizaokroshidze <liziokroshidze1994@gmail.com>
2024-11-06 16:01:09 +0400

    commit

---
+++ level10/classwork/cw1.py
@@ -0,0 +1,13 @@
+#input-შეყვანა / შეტანა
+#output-გამოყვანა / გამოტანა
+
+#name=input("თქვენი სახელი? ")
+#print("გამარჯობა,"+ name)
+
+
+#user_name = input("Enter your name: ")
+#print("hello " + "my name is goabot")
+#user_surname = input ("Enter your surname: ")
+
+
+
---
+++ level10/homework/hw2.py
@@ -0,0 +1,6 @@
+#მოცემული რიცხვის გამრავლება 10-ზე
+#დაწერე პროგრამა, რომელიც მომხმარებელს შემოატანინებს რიცხვს, დაპრინტავს ჯერ ამ რიცხვს და შემდეგ ამ რიცხვის ნამრავლი 10-ზე.
+
+number = int(input("enter a number: "))
+print("The number you entered is:",number)
+print("The number multipied by 10 is:",number * 10)
