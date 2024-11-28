ab6426f288e29ec6ad9775adaf4473fe7425e6b3
lizaokroshidze <liziokroshidze1994@gmail.com>
2024-11-29 01:05:40 +0400

    commit

--- level26/classwork/cw.py
+++ level26/classwork/cw.py
@@ -1,1 +1,7 @@
-my_list = ()
+my_list = ["liza", "anna", "niko", "ina"]
+
+index = 0
+for element in my_list:
+    if element == "niko":
+        print(index)
+    index += 1
---
+++ level26/classwork/cw1.py
@@ -0,0 +1,36 @@
+# index
+
+#my_list = ["liza", "anna", "niko", "ina"]
+
+# print(my_list.index("liza"))
+
+# my_list = ["liza", "anna", "niko", "ina"]
+
+# my_list.insert(1,"luka")
+
+# print(my_list)
+
+#insert
+
+
+# numbers1 = [1,2,3,4,5]
+# numbers2 =[6,7,8,9,10]
+
+# numbers1.extend(numbers2)
+
+# print(numbers1)
+# print(numbers2)
+
+# numbers1 = [1,2,3,4,5]
+# numbers2 =[6,7,8,9,10]
+
+# numbers1.extend(numbers2)
+# numbers2.extend(numbers1)
+
+# print(numbers2)
+
+
+
+
+
+
