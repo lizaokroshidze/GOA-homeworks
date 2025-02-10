132dc1dca357443866f2475bc05c483f207bf113
lizaokroshidze <liziokroshidze1994@gmail.com>
2025-02-10 21:01:04 +0400

    commit

---
+++ level64/classwork/script.js
@@ -0,0 +1,60 @@
+console.log("Liza Okroshidze")
+
+// variable 
+// camelCase
+
+// var num = 5
+// var name = "liza"
+
+// console.log(num)
+
+// 
+
+// console.log(typeof String(4))
+// console.log(typeof Number("8"))
+// console.log(typeof Boolean(1))
+
+// 
+
+// alert("!!!")
+
+// prompt("enter your name!")
+
+// var UserName = prompt("Enter Your Name!")
+// var UserSurname = prompt("Enter Your Surname!")
+// console.log("Hello " + UserName + " " + UserSurname)
+
+
+// var Number1 = prompt("Enter First Number!")
+// var Number2 = prompt("Enter Second Number!")
+
+// console.log( Number1 + Number2,Number1 - Number2, Number1 * Number2, Number1 / Number2)
+
+
+// var Name = alert("Welcome! ")
+// console.log(Name + alert)
+
+
+// var UserName = prompt("Enter Your Name!")
+// var UserSurname = prompt("Enter Your Surname!")
+// console.log("Hello " + UserName + " " + UserSurname)
+
+// var Number1 = Number (prompt( "Enter First Number!"))
+// var Number2 = Number (prompt("Enter Second Number!"))
+
+// console.log(
+//   "Addition: " + (Number1 + Number2),
+//   "Subtraction: " + (Number1 - Number2),
+//   "Multiplication: " + (Number1 * Number2),
+//   "Division: " + (Number1 / Number2)
+// )
+
+// var userName = prompt("Enter Your Name!")
+// alert("Welcome " + userName + "!")
+
+
+// console.log( 5 < 3 )
+// console.log( 3 < 5 )
+// console.log( 5 >= 7 )
+// console.log( 5 == "5")
+// console.log( 5 === "5")
