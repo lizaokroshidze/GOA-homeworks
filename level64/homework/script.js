d49a50c75429ec18dcc9cabfaa7b21f94e1f932b
lizaokroshidze <liziokroshidze1994@gmail.com>
2025-02-12 01:00:08 +0400

    commit

---
+++ level64/homework/script.js
@@ -0,0 +1,32 @@
+var a = 5;
+var b = 3;
+var c = 4;
+var d = 6;
+
+console.log(a + " + " + b + " = " + (a + b));
+console.log(a + " + " + c + " = " + (a + c));
+console.log(a + " + " + d + " = " + (a + d));
+console.log(b + " + " + c + " = " + (b + c));
+console.log(b + " + " + d + " = " + (b + d));
+console.log(c + " + " + d + " = " + (c + d));
+
+console.log(a + " - " + b + " = " + (a - b));
+console.log(a + " - " + c + " = " + (a - c));
+console.log(a + " - " + d + " = " + (a - d));
+console.log(b + " - " + c + " = " + (b - c));
+console.log(b + " - " + d + " = " + (b - d));
+console.log(c + " - " + d + " = " + (c - d));
+
+console.log(a + " * " + b + " = " + (a * b));
+console.log(a + " * " + c + " = " + (a * c));
+console.log(a + " * " + d + " = " + (a * d));
+console.log(b + " * " + c + " = " + (b * c));
+console.log(b + " * " + d + " = " + (b * d));
+console.log(c + " * " + d + " = " + (c * d));
+
+console.log(a + " / " + b + " = " + (a / b));
+console.log(a + " / " + c + " = " + (a / c));
+console.log(a + " / " + d + " = " + (a / d));
+console.log(b + " / " + c + " = " + (b / c));
+console.log(b + " / " + d + " = " + (b / d));
+console.log(c + " / " + d + " = " + (c / d));
