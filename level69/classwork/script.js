8f9d05868067da03625b6dd7e624014747ffe404
lizaokroshidze <liziokroshidze1994@gmail.com>
2025-02-23 14:55:55 +0400

    commit

---
+++ level69/classwork/script.js
@@ -0,0 +1,20 @@
+// let num = prompt("Enter number: ")
+// let i = 1
+
+// while (i <= num) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//         console.log(i)
+//     }
+//     i++
+// }
+
+
+
+for(let i=5; i < 20 ; i++){
+    if(i % 2 == 0){
+        console.log(i , "even")
+    }
+    else{
+        console.log(i , "odd")
+    }
+}
