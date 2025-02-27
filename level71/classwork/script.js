fb441c5a6bb763a7703edf30c004e55362df899b
lizaokroshidze <liziokroshidze1994@gmail.com>
2025-02-27 21:02:11 +0400

    commit

---
+++ level71/classwork/script.js
@@ -0,0 +1,30 @@
+
+
+
+// let number = Number(prompt("შეიყვანეთ რიცხვი:"))
+// let sum = 0
+
+// for (let i = 1; i <= number; i++) {
+//     if (i % 2 === 0) {
+//         sum += i
+//     }
+// }
+
+// console.log("ლუწი რიცხვების ჯამი: " + sum)
+
+
+
+
+// let myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// let sum = 0
+
+// for (let i = 0; i < myArray.length; i++) {
+//   sum += myArray[i]
+// }
+
+// console.log("ჯამი:", sum)  
+
+
+
+let myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
