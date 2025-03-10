4303b5b0bdcab84796bf69531768e604a2465f97
lizaokroshidze <liziokroshidze1994@gmail.com>
2025-03-10 23:17:51 +0400

    commit

---
+++ level75/classwork/script.js
@@ -0,0 +1,17 @@
+
+
+
+const myInfo = {
+    name: "Liza",
+    surname: "Okroshidze",   
+    age: 19,
+    hobby: "Music"
+}
+
+myInfo.group = "57"
+
+console.log(myInfo.name)
+console.log(myInfo.surname)
+console.log(myInfo.age)
+console.log(myInfo.hobby)
+console.log(myInfo.group)
---
+++ level76/classwork/script.js
@@ -0,0 +1,26 @@
+// DOM - documnent object model
+
+// let paragraph = document.getElementsByTagName("p");
+
+// paragraph[0].innerHTML = "Hello Liza"
+
+// paragraph[0].style.color = "red"
+
+// paragraph[1].style.backgroundColor = "hotpink"
+
+// paragraph[2].innerHTML = "Hello, Liza!"
+// paragraph[2].style.color = "blue"
+
+// function changeContent() {
+//     document.getElementById("p1").innerHTML = "Liza"
+// }
+
+let h1 = document.getElementsByTagName("h1")
+
+let counter = 0
+
+function increase() {
+    counter++
+    h1[0].innerHTML = counter
+}
+
