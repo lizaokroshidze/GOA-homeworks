e98c2f975d9aa9df0c1bf05c36dc0eabce687793
lizaokroshidze <liziokroshidze1994@gmail.com>
2025-03-16 11:30:25 +0400

    commit

---
+++ level77/classwork/script.js
@@ -0,0 +1,12 @@
+
+let userInfo ={
+    name: "Liza",
+    surname: "Okroshidze",
+    email: "lizaokroshidze@gmail.com",
+    sayHi(){
+        console.log("Hello")
+}
+}
+
+console.log(userInfo.email)
+console.log(userInfo[user])
