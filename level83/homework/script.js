70df20dbf8f11149daaf3b530850e6f61bb1286a
lizaokroshidze <liziokroshidze1994@gmail.com>
2025-03-31 17:27:13 +0400

    commit

---
+++ level83/homework/script.js
@@ -0,0 +1,24 @@
+
+function generateRandomColor() {
+    const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    return randomColor;
+}
+
+const button = document.querySelector('button');
+const colorSpan = document.querySelector('span');
+const main = document.querySelector('main');
+
+
+button.addEventListener('click', () => {
+    const newColor = generateRandomColor();
+    main.style.backgroundColor = newColor;
+    colorSpan.textContent = newColor;
+});
+
+
+let btnSound = new Audio('click.mp3');
+    
+document.getElementById('myButton').addEventListener('mousedown', function() {
+    btnSound.play();
+});
+
