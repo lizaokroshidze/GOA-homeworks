d51df5905605b17e2273b6b7258efa469e58b5fc
lizaokroshidze <liziokroshidze1994@gmail.com>
2025-05-07 21:53:46 +0400

    commit

--- level96/classwork/script.js
+++ level96/classwork/script.js
@@ -1,29 +1,8 @@
-// num % 2 === 0 ? console.log(`The ${num} is even`) : num === 4 ? console.log(`The num is ${num}`) : console.log(`The ${num} is odd`)
+const num = 4;
 
-const form = document.querySelector("form");
-
-// const defaultInfo = {
-//     email: "blabla@gmail.com",
-//     passsword:"123"
-// }
-
-
-
-form.addEventListener("submit", (e)=>{
-    e.preventDefault()
-
-    const entered = {
-        email: e.target.email.value,
-        password: e.target.password.value
-    }
-
-    const table = document.querySelector("table")
-    
-    table.innerHTML += `
-    <tr>
-        <td>${entered.email}</td>
-        <td>${entered.password} </td>
-    </tr>
-    `
-
-})
+if(num % 2 === 0){
+    console.log(`The ${num} is even`)
+}
+else{
+    console.log(`the${num} is odd`)
+}
