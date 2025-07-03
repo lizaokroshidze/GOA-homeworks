075de4b8c30bba55c78cdfd73abc753fe1a5eece
lizaokroshidze <liziokroshidze1994@gmail.com>
2025-07-03 23:53:08 +0400

    commit

--- level119/classwork/script.js
+++ level119/classwork/script.js
@@ -8,26 +8,21 @@ class Animal {
 const FoxAnimal = new Animal("Mr.fox", "ყვირის");
 const LionAnimal = new Animal("Mr.Lion", "Roar");
 
-// Map შექმნა
 let map = new Map([
   ["name", "liza"],
   ["surname", "Okroshidze"],
 ]);
 
-// Key-ების მნიშვნელობების ამოღება
 console.log(map.get("name"));
 console.log(map.get("surname"));
 console.log(map.has("surname")); 
 
-// ყველა key-value წყვილის გამოტანა
 for (let [key, value] of map.entries()) {
   console.log(key, value);
 }
 
-// ახალ key-value წყვილის დამატება
 map.set("hasLion", "mkavs!");
 
-// განახლებული Map-ის წაკითხვა
 for (let [key, value] of map.entries()) {
   console.log(key, value);
 }
