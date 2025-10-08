b4f11289d9bd20fa7673e7dd2b6381ac64765d18
lizaokroshidze <liziokroshidze1994@gmail.com>
2025-10-08 21:34:58 +0400

    commit

---
+++ level153/homework/github-rct/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <div className="bg-[#131c2f] w-full h-screen">
+        <div>
+          <h1 className="font-inconsolata text-white text-[30px] font-[400] tracking-[1px]"> DevFinder</h1>
+          <div>
+            Light 
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
